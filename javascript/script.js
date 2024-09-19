415edf1ebe3d135ccdaf186bb9d4cba5773a30f5
AlexLopEx03 <alejandrolopezexposito2003@gmail.com>
2024-09-19 16:09:23 +0100

    Inicializando dominio

---
+++ javascript/script.js
@@ -0,0 +1,16 @@
+console.log("Prueba log");
+console.warn("Prueba warn");
+console.debug("Prueba debug");
+console.info("Prueba info");
+console.error("Prueba error");
+
+console.groupCollapsed("Grupo de logs");
+console.log("Prueba log 1");
+console.log("Prueba log 2");
+console.groupEnd();
+
+console.log(document.body);
+
+console.trace("Hasta aqui e llegao");
+
+document.getElementById("video_fondo").style.display("none");
