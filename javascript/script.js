daf2ed52ba04d2c94b628f9a1f30c4f01e0dbbbe
AlexLopEx03 <alejandrolopezexposito2003@gmail.com>
2024-10-30 16:19:10 +0000

    Modificación completa del dominio

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,8 @@
-console.log("Prueba log");
-console.warn("Prueba warn");
-console.debug("Prueba debug");
-console.info("Prueba info");
-console.error("Prueba error");
-
-console.groupCollapsed("Grupo de logs");
-console.log("Prueba log 1");
-console.log("Prueba log 2");
-console.groupEnd();
-
-console.log(document.body);
-
-console.trace("Hasta aqui e llegao");
-
-document.getElementById("video_fondo").style.display("none");
+function menu(zona){
+    document.getElementById('id-inicio').style.display='none';
+    document.getElementById('id-proyectos').style.display='none';
+    document.getElementById('id-conocimientos').style.display='none';
+    document.getElementById('id-contacto').style.display='none';
+    document.getElementById('id-acercaDeMi').style.display='none';
+    document.getElementById(zona).style.display='flex';
+}
